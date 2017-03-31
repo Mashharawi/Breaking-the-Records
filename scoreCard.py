75e5a9991c512cd1baf689b67b6a42adf65b64d0
manasahs <manasazg@gmail.com>
2017-03-31 10:05:59 +0530

    Add files via upload

---
+++ scoreCard.py
@@ -0,0 +1,31 @@
+def highRecordBrk(b):
+    count = 0;
+    highScore = runs[0]
+    for i in range(0, b):
+        if runs[i]>highScore:
+            count += 1
+            highScore = runs[i]
+    return count
+
+def lowRecordBrk(b):
+    count = 0;
+    lowScore = runs[0]
+    for j in range(0, b):
+        if runs[j] < lowScore:
+            count +=  1
+            lowScore = runs[j]
+    return count
+
+#Generating Score Card
+runs = [ ]          #Empty list to update the runs scored
+print("Enter the number of games per series")
+games = input()
+print('Enter runs in each games')
+for i in range(0,games):
+    a = input()
+    runs.append(int(a))
+
+print('Runs scored so far')
+print(runs)
+print(highRecordBrk(games))
+print(lowRecordBrk(games))
